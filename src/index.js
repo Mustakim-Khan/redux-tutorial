50542b7dd0ba338cfb175a86122214e97915407b
Mustakim-Khan <mustakimzidan@gmail.com>
2020-12-24 13:01:09 +0100

    example redux finished

--- src/index.js
+++ src/index.js
@@ -4,9 +4,59 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import allReducers from './store/reducers'
+/*
+
+//STORE -> GLOBALIZED STATE
+
+
+//ACTION INCREMENT
+const increment = () => {
+  return{
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return{
+    type: 'DECREMENT'
+  }
+}
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+  }
+}
+
+let store = createStore(counter)
+
+//display store
+store.subscribe(() => console.log(store.getState()))
+
+//DISPATCH
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(increment())
+store.dispatch(increment())
+*/
+
+const store = createStore(
+  allReducers, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
